d0119fce927ee1896e6851b2b37dee95aa794213
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 17:41:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total = @total * (1 - @discount / 100)
+    @total = @total * (@discount / 100)
     puts "After the discount, the total comes to #{total}"
   end
 
